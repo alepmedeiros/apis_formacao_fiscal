495dfc2ea817809f7999a5b8a91982ab69ed1cfd
alepmedeiros <alemedeiros@bsd.com.br>
2024-12-07 06:07:16 -0300

    atualizado  buid.bat

--- build.bat
+++ build.bat
@@ -1,38 +1,32 @@
 @echo off
-:: Mostra o diretório atual para depuração
-echo Diretório atual: %cd%
-
 :: Configura as variáveis de ambiente do Delphi
 call "C:\Program Files (x86)\Embarcadero\Studio\23.0\bin\rsvars.bat"
 
 :: Verifica se os argumentos foram passados
-if "%1"=="" (
-    echo Erro: Caminho do diretorio do projeto nao especificado.
-    echo Uso: build.bat <CaminhoDoDiretorio> <NomeDoProjeto.dproj>
+if "%~1"=="" (
+    echo Erro: Caminho do diretório do projeto não especificado.
+    echo Uso: build.bat [CaminhoDoDiretorio] [NomeDoProjeto.dproj]
     exit /b 1
 )
 
-if "%2"=="" (
-    echo Erro: Nome do projeto nao especificado.
-    echo Uso: build.bat <CaminhoDoDiretorio> <NomeDoProjeto.dproj>
+if "%~2"=="" (
+    echo Erro: Nome do projeto não especificado.
+    echo Uso: build.bat [CaminhoDoDiretorio] [NomeDoProjeto.dproj]
     exit /b 1
 )
-
-:: Variáveis
-set DIRETORIO_PROJETO=%cd%\%1
-set PROJETO=%2
 
 :: Navega para o diretório do projeto
-cd %DIRETORIO_PROJETO%
-
-:: Mostra o diretório após navegar
-echo Diretório do projeto: %cd%
-
-:: Compilação usando MSBuild
-echo Compilando o projeto: %PROJETO%...
-msbuild %PROJETO% /p:Config=Release /p:Platform=Win32
+cd /d "%~1"
 if %errorlevel% neq 0 (
-    echo Erro ao compilar o projeto: %PROJETO%.
+    echo Erro: Não foi possível acessar o diretório do projeto: %~1.
+    exit /b %errorlevel%
+)
+
+:: Compilação do projeto
+echo Compilando o projeto %~2 para Linux...
+msbuild "%~2" /p:Config=Release /p:Platform=Linux64
+if %errorlevel% neq 0 (
+    echo Erro ao compilar o projeto: %~2.
     exit /b %errorlevel%
 )
 
